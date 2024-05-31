4c20f0dfdca2d5f10b16ba412ad07c8e26d54d69
SALAHEDDINE AHANSAL <salaheddineahansal@gmail.com>
2024-05-31 20:29:09 +0100

    Create Actions array of routes for navigation with data binding

--- VueApp/src/app/app.component.ts
+++ VueApp/src/app/app.component.ts
@@ -10,29 +10,13 @@ export class AppComponent {
 
   actions : Array<any> = [
     { title : "Home", "route" : "/home", icon: "house"},
-    { title : "Products", "route" : "/product", icon: "arrow-down-up"},
-    { title : "New Product", "route" : "/newProduct", icon: "plus-circle"},
-  ]
-
-
-  title = 'VueApp';
-
-  public currentRoute: any;
-
-  constructor(private router: Router) {
-
-  }
-  gotoHome(): void {
-    this.currentRoute= 'home';
-    this.router.navigateByUrl("/home");
-  }
-  gotProducts(): void {
-    this.currentRoute= 'products';
-    this.router.navigateByUrl("/products");
+    { title : "Products", "route" : "/products", icon: "arrow-down-up"},
+    { title : "New Product", "route" : "/newProdcut", icon: "plus-circle"},
+  ];
+  public currentActions: any;
+  setCurrentAction( action : any) {
+    this.currentActions = action ;
   }
 
-  gotNewProducts() {
-    this.currentRoute= 'newProdcut';
-    this.router.navigateByUrl("/newProdcut");
-  }
+
 }
